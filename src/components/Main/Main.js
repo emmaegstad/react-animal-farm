fe6dcfe66f85846ed40db768a3bc0f7b45cd0c67
Emma Hardin <emma@Emmas-MacBook-Pro-2.local>
2021-11-29 15:33:54 -0800

    oops forgot about commits

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import './Main.css'
+import Animal from '../Animal/Animal'
+import Background from '../../background.png'
+import { animals } from '../../data.js'
 
 export default function Main() {
-  return <main></main>
+  return (
+    <main style={{ backgroundImage: `url(${Background})` }}>
+      {animals.map((element) => {
+        return (
+          <Animal
+            className=".animal .name img"
+            key={element.name}
+            name={element.name}
+            type={element.type}
+            says={element.says}
+            top={element.top}
+            left={element.left}
+          />
+        )
+      })}
+    </main>
+  )
 }
